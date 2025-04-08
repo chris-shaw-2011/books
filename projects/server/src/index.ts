a9ec1f95a46fe74b2d5d7e7ee3b131fca537be4c
Chris Shaw <chris@christopher-shaw.com>
2025-04-08 14:05:19 -0400

    Handle shutdown signals

--- projects/server/src/index.ts
+++ projects/server/src/index.ts
@@ -7,3 +7,26 @@ await db.open()
 void bookList.loadBooks()
 
 await server.start()
+
+const shutdown = async () => {
+	// eslint-disable-next-line no-console
+	console.log("Received shutdown signal")
+	await server.stop()
+
+	// eslint-disable-next-line no-console
+	console.log("Closing db connection...")
+
+	await db.close() // if your db module supports clean shutdown
+
+	// eslint-disable-next-line no-console
+	console.log("Db connection closed.")
+
+	// eslint-disable-next-line no-console
+	console.log("Exiting program...")
+	process.exit(0)
+}
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+process.on("SIGINT", shutdown)
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+process.on("SIGTERM", shutdown)
